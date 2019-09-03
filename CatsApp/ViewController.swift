0a3a049fcc9134f9abc5e6dd1afd9c9b53099074
spedro <spedro@thoughtworks.com>
2019-09-03 18:32:33 -0300

    Initial Commit

---
+++ CatsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatsApp
+//
+//  Created by Simao Pedro Gomes Teixeira on 03/09/19.
+//  Copyright © 2019 Simao Pedro Gomes Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
