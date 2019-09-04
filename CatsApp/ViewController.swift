693c3f3b4b8e7a809668ac2f2d8c7fe63edb0c71
spedro <spedro@thoughtworks.com>
2019-09-04 13:47:05 -0300

    initial commit

--- CatsApp/ViewController.swift
+++ CatsApp/ViewController.swift
@@ -10,10 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var catPhoto: UIImageView!
+    
+    @IBAction func didTapCalculate(_ sender: UIButton) {
+        let age = ageTextField.text
+        let intAge = Int(age!)!
+        let catAge = intAge * 7
+        catAgeResult.text = "Seu gato tem \(catAge) anos!"
+        
+        if catAge < 10 {
+            catPhoto.image = UIImage(named: "babycat")
+        } else if catAge < 70 {
+            catPhoto.image = UIImage(named: "normalCat2")
+        } else if catAge > 70 {
+            catPhoto.image = UIImage(named: "oldCat2")
+        }
+        
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBOutlet weak var catAgeResult: UILabel!
+    
 
 
 }
